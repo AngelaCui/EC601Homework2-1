540acac6ed52bb5e88578c1a201498034db28ab2
sbobhate <sbobhate@bu.edu>
2016-09-30 14:54:03 -0400

    Done Building First App
    Successfully followed all steps of the tutorial to make a simple
    application that sends a string across from one activity to another.

---
+++ OurFirstApp/app/src/main/java/com/example/ourfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.ourfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.ourfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+}
--- OurFirstApp/app/src/main/java/com/example/ourfirstapp/MainActivity.java
+++ OurFirstApp/app/src/main/java/com/example/ourfirstapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.ourfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+        Key to send and receive data across activities
+        Good practise to use package name a prefix since different apps may interacts
+     */
+    public final static String EXTRA_MESSAGE = "com.example.ourfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+        Called when the user click on the Send Button
+     */
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
 }
+
